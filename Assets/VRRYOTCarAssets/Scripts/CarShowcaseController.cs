479c361622661434557ee4de26ef7227822d9af1
shollaaatwit <shollaa@wit.edu>
2020-11-10 21:22:10 -0500

    Made some changes to my scene - alex s

---
+++ Assets/VRRYOTCarAssets/Scripts/CarShowcaseController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarShowcaseController : MonoBehaviour
+{
+    public GameObject start;
+
+    public static GameObject targetOne;
+    public static GameObject targetTwo;
+    public static GameObject targetThree;
+    public static GameObject targetFour;
+
+    public GameObject[] targets = new GameObject[] {targetOne, targetTwo, targetThree, targetFour};
+    
+
+    public static GameObject carOne;
+    public static GameObject carTwo;
+    public static GameObject carThree;
+    public static GameObject carFour;
+
+    public GameObject[] cars = new GameObject[] {carOne, carTwo, carThree, carFour};
+
+    public float constantSpeed;
+    public float fastSpeed;
+
+    void Update()
+    {
+    }
+
+    public void CarRotation()
+    {
+        transform.RotateAround(start.transform.position, Vector3.up, constantSpeed*Time.deltaTime);   
+
+    }
+    public void MoveCarToSpot()
+    {
+
+    }
+}
---
+++ Assets/VRRYOTCarAssets/Scripts/SkyboxController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentController : MonoBehaviour
+{
+
+    public static Material matOne;
+    public static Material matTwo;
+    public static Material matThree;
+    public int number;
+    
+    public Material[] skyboxes = new Material[] {matOne, matTwo, matThree}; // holds array of skybox materials
+
+    void Start() //changes skybox to determined material upon activation
+    {
+        SetEnvironment(number);
+    }
+
+    public void SetEnvironment(int number) // changes skybox
+    {
+        RenderSettings.skybox = skyboxes[number];
+    }
+}
