2c6932c0cf4ea02b0d95364ba45b0de8e91bc8d0
pedro9867 <pferreira9867@gmail.com>
2020-11-12 16:38:23 -0500

    tablet first push

---
+++ Assets/CarsController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarsController : MonoBehaviour
+{
+    public GameObject[] carPrefabs;
+    public Transform spawnLocation;
+
+
+
+    private void Start()
+    {
+
+    }
+
+    public void SpawnCar(int carNum)
+   {
+        Instantiate(carPrefabs[carNum], spawnLocation);
+   }
+
+  public void ChangeCar(int carNum)
+    {
+        foreach (GameObject car in carPrefabs)
+        {
+            GameObject.Destroy(car);
+        }
+        SpawnCar(carNum);
+    }
+}
