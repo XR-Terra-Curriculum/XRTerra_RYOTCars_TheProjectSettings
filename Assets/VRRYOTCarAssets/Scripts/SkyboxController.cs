a3f6a4dfd4493c90977722ab3381fc9358299e80
shollaaatwit <shollaa@wit.edu>
2020-11-15 18:01:23 -0500

    Skybox controller implementation

--- Assets/VRRYOTCarAssets/Scripts/SkyboxController.cs
+++ Assets/VRRYOTCarAssets/Scripts/SkyboxController.cs
@@ -5,20 +5,25 @@ using UnityEngine;
 public class SkyboxController : MonoBehaviour
 {
 
+    public Light light; 
+    public int number;
+
     public static Material matOne;
     public static Material matTwo;
     public static Material matThree;
-    public int number;
+
+    private static Color daytime = new Color(1f, 0.9568627f, 0.8392157f, 1f);
+    private static Color evening = new Color(1f, 0.7836223f, 0.4575472f, 1f);
+    private static Color night = new Color (0.4528302f, 0.4528302f, 0.4528302f, 1f);
+
+
     
     public Material[] skyboxes = new Material[] {matOne, matTwo, matThree}; // holds array of skybox materials
+    public Color[] lightColors = new Color[] {daytime, evening, night}; // holds different light colors
 
-    void Start() //changes skybox to determined material upon activation
-    {
-        SetEnvironment(number);
-    }
-
-    public void SetEnvironment(int number) // changes skybox
+    public void SetEnvironment(int number) // changes skybox and lighting
     {
         RenderSettings.skybox = skyboxes[number];
+        light.color = lightColors[number];
     }
 }
